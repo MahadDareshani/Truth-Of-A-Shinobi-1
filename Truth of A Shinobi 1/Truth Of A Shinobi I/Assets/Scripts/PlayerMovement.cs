5fe439450d9a3c5e01e1c7fe01a92b293a59345b
MahadDareshani <53760266+MahadDareshani@users.noreply.github.com>
2019-09-29 10:20:36 +0500

    Controller for player
    The Scene is inside the PROTOTYPE folder

---
+++ Truth of A Shinobi 1/Truth Of A Shinobi I/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed;
+    bool facingRight;
+    public float jumpForce;
+    private Rigidbody2D rb;
+    private bool isGrounded;
+    public Transform groundCheck;
+    public float checkRadius;
+    public LayerMask whatIsGround;
+    public int extraJumps;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if (isGrounded == true)
+        {
+            extraJumps = 2;
+        }
+
+        if (Input.GetKeyDown(KeyCode.W) && extraJumps > 0)
+        {
+            rb.velocity = Vector2.up * jumpForce;
+            extraJumps--;
+        }
+        else if (Input.GetKeyDown(KeyCode.W) && extraJumps == 0 && isGrounded == true)
+        {
+            rb.velocity = Vector2.up * jumpForce;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position = transform.position += transform.right * -moveSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position = transform.position += transform.right * moveSpeed * Time.deltaTime;
+        }
+
+        if (moveSpeed > 0 && !facingRight)
+        {
+            flip();
+        }
+        else if (moveSpeed < 0 && facingRight)
+        {
+            flip();
+        }
+    }
+
+    void FixedUpdate()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position,checkRadius,whatIsGround);
+    }
+
+    void flip()
+
+    {
+
+        facingRight = !facingRight;
+        Vector2 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+
+    }
+}
